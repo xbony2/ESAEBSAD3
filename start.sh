337ca682300a3b5d3bd126f7862c240973df6ea1
Eric Schneider <xbony22@gmail.com>
2021-01-02 21:43:40 -0500

    Added startup script because I can't remember how to do it.

---
+++ start.sh
@@ -0,0 +1,9 @@
+nohup ruby run.rb &
+
+# Note:
+# To kick the process, try:
+# ps -ef | grep ruby
+# and then
+# kill <PID>
+# (I should automate this probably. Using !stop is fine as it is though.)
+
