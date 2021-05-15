7cd67fbae323be0892ca55152e03905d8f2caf10
Eric Schneider <xbony22@gmail.com>
2021-05-14 21:40:20 -0400

    GH Actions fixed for now

--- lib/esaebsad3/esaebsad3.rb
+++ lib/esaebsad3/esaebsad3.rb
@@ -10,7 +10,7 @@ require_rel 'commands'
 module ESAEBSAD3
 	CONFIG = Tomlrb.load_file('config.toml').freeze
 
-	BOT = Discordrb::Commands::CommandBot.new(token: CONFIG['discord-token'], client_id: CONFIG['discord-client'], prefix: CONFIG['discord-prefix']) 
+	BOT = Discordrb::Commands::CommandBot.new(token: CONFIG['discord-token'], client_id: CONFIG['discord-client'], prefix: CONFIG['discord-prefix'])
 
 	# TODO: `assertion: :bot`
 	WIKI = MediaWiki::Butt.new('https://ftb.fandom.com', assertion: :bot)
