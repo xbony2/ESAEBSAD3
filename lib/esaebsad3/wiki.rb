cd1f2d42d7a59d9342f2900d705ee99dbad8e0fe
Eric Schneider <xbony22@gmail.com>
2022-05-29 19:38:17 -0400

    this seems to be working quite well, so i will save it

--- lib/esaebsad3/wiki.rb
+++ lib/esaebsad3/wiki.rb
@@ -1,14 +1,27 @@
 module ESAEBSAD3
 	module Wiki
+		def self.new_group_id
+			last_group_id = BOT_DB.exec(%{SELECT MAX(groupid) FROM actions;}).getvalue(0, 0)
+			last_group_id = last_group_id.nil? ? -1 : last_group_id.to_i
+
+			new_group_id = last_group_id + 1
+			new_group_id
+		end
+
 		def self.edit(title, text, opts = {})
+			edit_with_group(title, text, new_group_id, opts)
+		end
+
+		def self.edit_with_group(title, text, group_id, opts = {})
 			before_text = WIKI.get_text(title)
 
-			# wip: experimenting directly with the DB rn before doing shit in ruby -bony
-			last_group_id = BOT_DB.exec('SELECT')
+			id = BOT_DB.exec_params(%{INSERT INTO actions(groupid, wikitext_before, wikitext_after, type, complete) VALUES($1, $2::text, $3::text, 'edit', false) RETURNING id;}, [group_id, before_text, text]).getvalue(0, 0)
 
-			# BOT_DB.exec_params('INSERT INTO actions() VALUES()')
+			p "id: #{id}"
 
 			WIKI.edit(title, text, opts)
+
+			BOT_DB.exec_params(%{UPDATE actions SET complete = true WHERE id = $1}, [id])
 
 			puts "did an edit!"
 		end
