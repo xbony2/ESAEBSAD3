4a5dcfd4ee3898463bb44606d074e78065791a86
Eric Schneider <xbony22@gmail.com>
2022-05-29 20:49:21 -0400

    migrate gen_land_cats to use actions

--- lib/esaebsad3/wiki.rb
+++ lib/esaebsad3/wiki.rb
@@ -8,10 +8,10 @@ module ESAEBSAD3
 		end
 
 		def self.edit(command, title, text, opts = {})
-			edit_with_group(command, title, text, new_group_id, opts)
+			edit_with_group(new_group_id, command, title, text, opts)
 		end
 
-		def self.edit_with_group(command, title, text, group_id, opts = {})
+		def self.edit_with_group(group_id, command, title, text, opts = {})
 			before_text = WIKI.get_text(title)
 
 			id = BOT_DB.exec_params(%(INSERT INTO actions(groupid, command, wikitext_before, wikitext_after, type, complete) VALUES($1, $2::text, $3::text, $4::text, 'edit', false) RETURNING id;), [group_id, command, before_text, text]).getvalue(0, 0)
@@ -19,6 +19,18 @@ module ESAEBSAD3
 			WIKI.edit(title, text, opts)
 
 			BOT_DB.exec_params(%(UPDATE actions SET complete = true WHERE id = $1), [id])
+		end
+
+		def self.create_page(command, title, text, opts = {})
+			create_page_with_group(new_group_id, command, title, text, opts)
+		end
+
+		def self.create_page_with_group(group_id, command, title, text, opts = {})
+			id = BOT_DB.exec_params(%(INSERT INTO actions(groupid, command, wikitext_after, type, complete) VALUES($1, $2::text, $3::text, 'create', false) RETURNING id;), [group_id, command, text]).getvalue(0, 0)
+
+			WIKI.edit(title, text, opts)
+
+			BOT_DB.create_page(%(UPDATE actions SET complete = true WHERE id = $1), [id])
 		end
 	end
 end
